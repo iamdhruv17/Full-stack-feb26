30916404107ef539afd24e8124052a7d0d773d89
iamdhruv17 <dsinghal7834@gmail.com>
2026-02-19 15:10:30 +0530

    19-02-2026

--- Button/src/App.jsx
+++ Button/src/App.jsx
@@ -1,14 +1,14 @@
-
-import './App.css'
-import EventToDo from './EventToDo'
-// import EventTodo from './EventTodo'
-// import Counter from './Counter'
+import "./App.css"
+import EventToDo from "./EventToDo" 
+// import Counter from "./Counter.jsx"
+import Search from "./Search.jsx"
 function App() {
-  
-
+  // let name="Divyansh Kaushik";
   return (
     <>
-     <EventToDo />
+    <Search/>
+    {/* <EventToDo/> */}
+    {/* <Counter/> */}
     </>
   )
 }
