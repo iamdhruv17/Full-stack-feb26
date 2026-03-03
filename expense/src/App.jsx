bf4b44ccebb7d8e886e81ad5e0e30bc0677d74a1
iamdhruv17 <dsinghal7834@gmail.com>
2026-03-03 19:59:45 +0530

    03-03-2026

--- expense/src/App.jsx
+++ expense/src/App.jsx
@@ -20,8 +20,8 @@ function App() {
       <li>Secure Data Storage: Your data is securely stored and backed up.</li>   
       <li>Customizable Categories: Create custom categories for better organization.</li>
       <li>Recurring Expenses: Set up recurring expenses for regular bills.</li>
-      <li>Export Data: Export your expense data in CSV format for further analysis.</li>  
-      <li>Mobile App: Access your expenses on the go with our mobile app.</li>  
+      {/* <li>Export Data: Export your expense data in CSV format for further analysis.</li>   */}
+      {/* <li>Mobile App: Access your expenses on the go with our mobile app.</li>   */}
     </ul>
 
     
