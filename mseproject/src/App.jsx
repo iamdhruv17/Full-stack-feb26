14e9b5b1fee73074570e72f11825b0043fcb55e9
iamdhruv17 <dsinghal7834@gmail.com>
2026-03-19 15:05:02 +0530

    19-03-2026

---
+++ mseproject/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './component/Header'
+import Footer from './component/Footer'
+// import axios from 'axios'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Welcome to My Website</h1>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ mseproject/src/component/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <footer>
+
+            <footer>Copyright © 2024 Student Help. All rights reserved.</footer>
+            <footer>Contact us at info@studenthelp.com</footer>
+        </footer>
+    );
